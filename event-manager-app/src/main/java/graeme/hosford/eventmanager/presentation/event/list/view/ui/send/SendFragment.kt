38c299d395a35d48b58c5b545444af97bc07ff22
Graeme Hosford <graemehosford@gmail.com>
2020-01-16 15:53:27 +0000

    Fixed errors in Navigation placeholder fragments to allow for building app. These will mostly be deleted eventually

--- event-manager-app/src/main/java/graeme/hosford/eventmanager/presentation/event/list/view/ui/send/SendFragment.kt
+++ event-manager-app/src/main/java/graeme/hosford/eventmanager/presentation/event/list/view/ui/send/SendFragment.kt
@@ -8,7 +8,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import graeme.hosford.eventmanager.presentation.event.list.view.R
+import graeme.hosford.eventmanager.R
 
 class SendFragment : Fragment() {
 
