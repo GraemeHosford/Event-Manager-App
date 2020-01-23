68bbdd0e85ccb4acdf1650e3bd6f5229b9a4c525
Graeme Hosford <graemehosford@gmail.com>
2020-01-23 15:39:03 +0000

    Removed entity module

--- event-manager-entity/src/main/java/graeme/hosford/event/manager/entity/feature1/Entity1.kt
+++
@@ -1,5 +0,0 @@
-package graeme.hosford.event.manager.entity.feature1
-
-import graeme.hosford.event.manager.entity.BusinessEntity
-
-class Entity1: BusinessEntity
--- event-manager-entity/src/main/java/graeme/hosford/event/manager/entity/feature1/Entity1List.kt
+++
@@ -1,5 +0,0 @@
-package graeme.hosford.event.manager.entity.feature1
-
-import graeme.hosford.event.manager.entity.BusinessEntity
-
-data class Entity1List(val entities: Entity1) : BusinessEntity
