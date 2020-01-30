81136193f12c5e6262e379e823c941ff44c8eef5
Graeme Hosford <graemehosford@gmail.com>
2020-01-30 13:38:33 +0000

    Updated LoginPresenterImpl.kt to move interactors to nested

--- event-manager-app/src/main/java/graeme/hosford/eventmanager/business/login/LoginInteractorImpl.kt
+++ event-manager-app/src/main/java/graeme/hosford/eventmanager/business/login/LoginInteractorImpl.kt
@@ -1,11 +1,14 @@
 package graeme.hosford.eventmanager.business.login
 
 import graeme.hosford.eventmanager.business.common.BaseInteractor
+import graeme.hosford.eventmanager.business.common.Interactor
+import graeme.hosford.eventmanager.business.user.CurrentUserInteractor
 import graeme.hosford.eventmanager.data.login.CurrentUserNetworkAccess
 import javax.inject.Inject
 
 class LoginInteractorImpl @Inject constructor(
-    private val currentUserNetworkAccess: CurrentUserNetworkAccess
+    private val currentUserNetworkAccess: CurrentUserNetworkAccess,
+    private val currentUserInteractor: CurrentUserInteractor
 ) : BaseInteractor<LoginInteractor.SaveUserDetailsListener>(), LoginInteractor {
 
     override fun onCreate() {
@@ -13,8 +16,18 @@ class LoginInteractorImpl @Inject constructor(
         currentUserNetworkAccess.setUserInfoSavedListener(UserInfoSavedListener())
     }
 
+    override fun registerManagedInteractors(): List<Interactor<*>> = listOf(currentUserInteractor)
+
     override fun loggedIn(): Boolean {
         return currentUserNetworkAccess.getCurrentUser() != null
+    }
+
+    override fun registerCurrentUserInteractorCallback(listener: CurrentUserInteractor.UserCompanyListener) {
+        currentUserInteractor.registerCallback(listener)
+    }
+
+    override fun checkUserHasCompany() {
+        currentUserInteractor.checkUserHasCompany()
     }
 
     override fun saveUserEmail(email: String?) {
