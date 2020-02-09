ff5d14634d9ca36e79e7e85563c84463d8f2500e
Graeme Hosford <graemehosford@gmail.com>
2020-02-09 17:48:13 +0000

    Fixed error in test

--- event-manager-app/src/test/java/graeme/hosford/eventmanager/presentation/event/list/presentation/EventListPresenterImplTest.kt
+++ event-manager-app/src/test/java/graeme/hosford/eventmanager/presentation/event/list/presentation/EventListPresenterImplTest.kt
@@ -60,8 +60,8 @@ class EventListPresenterImplTest {
     @Test
     fun eventListProcessingCallback_showsData_onSuccess() {
         val models = listOf(
-            EventListItemUiModel("Event 1", "Desc 1", "Loc 1"),
-            EventListItemUiModel("Event 2", "Desc 2", "Loc 2")
+            EventListItemUiModel("Event 1", "Desc 1", "Loc 1", arrayListOf()),
+            EventListItemUiModel("Event 2", "Desc 2", "Loc 2", arrayListOf())
         )
 
         processorCapture.captured.onProcessingComplete(models)
