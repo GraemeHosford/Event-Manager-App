23df2f493f4d8eea47ef7a3f8447f883890b9719
Graeme Hosford <graemehosford@gmail.com>
2020-03-01 15:55:05 +0000

    Moved AttendeesAdapter.kt over to view binding

--- event-manager-app/src/main/java/graeme/hosford/eventmanager/presentation/attendees/view/adapter/AttendeesAdapter.kt
+++ event-manager-app/src/main/java/graeme/hosford/eventmanager/presentation/attendees/view/adapter/AttendeesAdapter.kt
@@ -3,11 +3,8 @@ package graeme.hosford.eventmanager.presentation.attendees.view.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-import butterknife.BindView
-import butterknife.ButterKnife
 import graeme.hosford.eventmanager.R
+import graeme.hosford.eventmanager.databinding.AttendeeItemLayoutBinding
 import graeme.hosford.eventmanager.presentation.attendees.model.AttendeesUiModel
 import graeme.hosford.eventmanager.presentation.common.view.ViewExtensions.isVisible
 import graeme.hosford.eventmanager.presentation.common.view.recyclerview.BaseAdapter
@@ -31,22 +28,16 @@ class AttendeesAdapter(private val clickListener: AttendeeClickListener) :
 class AttendeesViewHolder(itemView: View, private val clickListener: AttendeeClickListener) :
     BaseViewHolder<AttendeesUiModel>(itemView) {
 
-    @BindView(R.id.member_name_text_view)
-    lateinit var personName: TextView
-
-    @BindView(R.id.attendee_checkmark)
-    lateinit var attendingCheckmark: ImageView
-
     override fun bind(model: AttendeesUiModel) {
-        ButterKnife.bind(this, itemView)
+        val binding = AttendeeItemLayoutBinding.bind(itemView)
 
-        personName.text = model.name
+        binding.attendeeItemLayoutIncludedName.memberNameTextView.text = model.name
 
         itemView.setOnClickListener {
-            if (attendingCheckmark.isVisible()) {
-                attendingCheckmark.visibility = View.INVISIBLE
+            if (binding.attendeeCheckmark.isVisible()) {
+                binding.attendeeCheckmark.visibility = View.INVISIBLE
             } else {
-                attendingCheckmark.visibility = View.VISIBLE
+                binding.attendeeCheckmark.visibility = View.VISIBLE
             }
 
             /* TODO: As of now name is essentially just email and will serve as a unique ID.
