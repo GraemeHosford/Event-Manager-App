4ab7bfad54490072f325bc7ebace67bf2f5320b7
Graeme Hosford <graemehosford@gmail.com>
2020-03-21 21:05:29 +0000

    Cut out some unnecessary code in EventEntityConverter.kt by making function getOrDefault more generic

--- event-manager-app/src/main/java/graeme/hosford/eventmanager/data/event/list/converter/EventEntityConverter.kt
+++ event-manager-app/src/main/java/graeme/hosford/eventmanager/data/event/list/converter/EventEntityConverter.kt
@@ -6,28 +6,29 @@ import graeme.hosford.eventmanager.entity.event.Event
 import java.util.*
 import javax.inject.Inject
 
+@Suppress("UNCHECKED_CAST")
 class EventEntityConverter @Inject constructor() : EntityConverter<Event> {
 
     override fun convert(document: DocumentSnapshot): Event {
         with(document.data) {
             return Event(
                 document.id,
-                getOrDefault(this, Event.OWNER, "") as String,
-                getOrDefault(this, Event.NAME_FIELD, "") as String,
-                getOrDefault(this, Event.DESCRIPTION_FIELD, "") as String,
+                getOrDefault(this, Event.OWNER, ""),
+                getOrDefault(this, Event.NAME_FIELD, ""),
+                getOrDefault(this, Event.DESCRIPTION_FIELD, ""),
                 getCalendarFromLong(document.getLong(Event.START_DATE) ?: 0L),
                 getCalendarFromLong(document.getLong(Event.END_DATE) ?: 0L),
-                getOrDefault(this, Event.LOCATION_NAME_FIELD, "") as String,
-                getOrDefault(this, Event.LOCATION_LAT_FIELD, 0.0) as Double,
-                getOrDefault(this, Event.LOCATION_LONG_FIELD, 0.0) as Double,
-                getOrDefault(this, Event.INVITEES_LIST, arrayListOf<String>()) as ArrayList<String>,
-                getOrDefault(this, Event.ATTENDEES_LIST, arrayListOf<String>()) as ArrayList<String>
+                getOrDefault(this, Event.LOCATION_NAME_FIELD, ""),
+                getOrDefault(this, Event.LOCATION_LAT_FIELD, 0.0),
+                getOrDefault(this, Event.LOCATION_LONG_FIELD, 0.0),
+                getOrDefault(this, Event.INVITEES_LIST, arrayListOf()),
+                getOrDefault(this, Event.ATTENDEES_LIST, arrayListOf())
             )
         }
     }
 
-    private fun getOrDefault(data: MutableMap<String, Any>?, key: String, defaultValue: Any): Any =
-        data?.get(key) ?: defaultValue
+    private fun <T> getOrDefault(data: MutableMap<String, Any>?, key: String, defaultValue: T): T =
+        data?.get(key) as T ?: defaultValue
 
     private fun getCalendarFromLong(millis: Long): Calendar {
         val cal = Calendar.getInstance()
