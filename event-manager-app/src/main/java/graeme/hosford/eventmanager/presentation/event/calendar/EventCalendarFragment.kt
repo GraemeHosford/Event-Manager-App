4dfe43d2899ed08700ac2927e740f502e3355503
Graeme Hosford <graemehosford@gmail.com>
2020-04-09 13:46:36 +0100

    Basics of calendar fragment added

---
+++ event-manager-app/src/main/java/graeme/hosford/eventmanager/presentation/event/calendar/EventCalendarFragment.kt
@@ -0,0 +1,31 @@
+package graeme.hosford.eventmanager.presentation.event.calendar
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import graeme.hosford.eventmanager.R
+import graeme.hosford.eventmanager.databinding.FragmentEventCalendarLayoutBinding
+import graeme.hosford.eventmanager.presentation.common.view.fragment.BaseFragment
+import graeme.hosford.eventmanager.presentation.event.list.attending.view.EventListAttendingFragment
+
+class EventCalendarFragment : BaseFragment() {
+
+    private var binding: FragmentEventCalendarLayoutBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentEventCalendarLayoutBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        childFragmentManager.beginTransaction()
+            .replace(R.id.event_list_fragment_container, EventListAttendingFragment())
+    }
+}
